f19af899bd135baf8a6ce6750ed3255d594e4e96
Chargeuprepository <azuredatacenter@echargeup.com>
2025-04-19 15:16:35 +0530

    new data

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 async function game() {
   const res = await fetch(
-    "https://raw.githubusercontent.com/Chargeuprepository/temp-OEM-data/refs/heads/master/data.json"
+    "https://raw.githubusercontent.com/Chargeuprepository/temp-OEM-data/refs/heads/master/data.json?timestamp=" +
+      new Date().getTime()
   );
   const ans = await res.json();
 
